fa7ea1b348306fe7194c839f176cc09a16c2a9a3
maaxg <maxgps5@hotmail.com>
2020-03-20 14:54:29 -0300

    Insert user with POST

--- src/main/java/com/example/maaxg/workshopmongo/services/UserService.java
+++ src/main/java/com/example/maaxg/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.maaxg.workshopmongo.services;
 
 import com.example.maaxg.workshopmongo.domain.User;
+import com.example.maaxg.workshopmongo.dto.UserDTO;
 import com.example.maaxg.workshopmongo.repository.UserRepository;
 import com.example.maaxg.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-
-
-
     }
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
+
 
 }
