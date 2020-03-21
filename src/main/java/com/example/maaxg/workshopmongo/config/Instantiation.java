1dd5a5729ace7685b67ca177c9b81d16b3d1721c
maaxg <maxgps5@hotmail.com>
2020-03-21 15:35:15 -0300

    Projecting data from author with DTO

--- src/main/java/com/example/maaxg/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/maaxg/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.example.maaxg.workshopmongo.config;
 
 import com.example.maaxg.workshopmongo.domain.Post;
 import com.example.maaxg.workshopmongo.domain.User;
+import com.example.maaxg.workshopmongo.dto.AuthorDTO;
 import com.example.maaxg.workshopmongo.repository.PostRepository;
 import com.example.maaxg.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +28,12 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo abraços.", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei Feliz Hoje", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo abraços.", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei Feliz Hoje", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
